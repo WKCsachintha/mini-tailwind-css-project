a927325d405fc910e2a8c0506e6714dbd4065b63
WKCsachintha <wkcsachintha@students.nsbm.ac.lk>
2025-06-30 17:15:15 +0530

    add project file

--- PlanNest/src/App.jsx
+++ PlanNest/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 
 import Header from './components/Header'
 import About from './components/About'
+import Projects from './components/Projects'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <div className='w-full overflow-hidden'>
       <Header/>
       <About/>
+      <Projects/>
     </div>
      
      
---
+++ PlanNest/src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='container mx-auto py-4 pt-20 px-20 lg:px-32 my-20 w-full
+      overflow-hidden' id='Project'>
+        <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects 
+            <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
+        <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Crafing Spaces,Building Legacies_explore Our Porfolio</p>
+    </div>
+  )
+}
+
+export default Projects
